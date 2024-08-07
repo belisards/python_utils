71370c9d1cb731f0b655efcb3e1d0448cdbc7bd3
belisards <adrianobf@gmail.com>
2024-08-07 14:49:01 -0300

    add wordclouds

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='handy_functions',
+    version='0.1',
+    packages=find_packages(),
+#    install_requires=[
+#        'matplotlib',
+#        'wordcloud',
+        # Add other dependencies here
+#    ],
+    author='Adriano Belisario',
+#    author_email='your.email@example.com',
+    description='A package for handy Python functions.',
+#    url='https://github.com/your-username/handy_functions',
+)
